2ed5b26012c2b5dedfe1d04a6ced1d34518b5a81
Ankush263 <ankushbanik263@gmail.com>
2022-12-09 16:37:04 +0530

    update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,16 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  const TokenName = "StreaX"
+  const TokenSymbol = "STX"
+  const TotalSupply = 1000
+  const Decimal = 18
+  const CustomeERC20 = await ethers.getContractFactory("CustomeERC20")
+  const customeERC20 = await CustomeERC20.deploy(TokenName, TokenSymbol, TotalSupply, Decimal)
+  await customeERC20.deployed()
+  console.log(
+    `Your token ${TokenSymbol} is deployed to the address ${customeERC20.address}`
+  )
 }
 
 // We recommend this pattern to be able to use async/await everywhere
